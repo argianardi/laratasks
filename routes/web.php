c342f53ea9ed58cafbef6883c8ffe82ec2c1c5b0
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-05-11 00:10:09 +0700

    Feat: Add basic controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\Halo\HaloController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Dari Router ke View
+Route::get('/router-view', function () {
+    return view('basic.try');
+});
+
+// Dari Router ke Controller
+Route::get('/router-controller', [HaloController::class, 'index']);
+
+// Dari Router ke Controller ke View
+Route::get('/router-controller-view', [HaloController::class, 'routerControllerView']);
+
+Route::get('/todo', function () {
+    return view('todo.app');
 });
