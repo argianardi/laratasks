5cbd888cba1c8e60faca1d8d255f4e2c11c1cba0
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-05-12 21:14:54 +0700

    Fix: Fix ui for search feature

--- resources/views/todo/app.blade.php
+++ resources/views/todo/app.blade.php
@@ -6,21 +6,21 @@
         <div class="container-fluid col-md-7">
             <div class="navbar-brand">Simple To Do List</div>
             <!--
-                                                                                                                                                <div class="navbar-collapse justify-content-end" id="navbarNav">
-                                                                                                                                                    <ul class="navbar-nav">
-                                                                                                                                                        <li class="nav-item dropdown">
-                                                                                                                                                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                                                                                                                                                                data-bs-toggle="dropdown" aria-expanded="false">
-                                                                                                                                                                Akun Saya
-                                                                                                                                                            </a>
-                                                                                                                                                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                                                                                                                                                <li><a class="dropdown-item" href="#">Logout</a></li>
-                                                                                                                                                                <li><a class="dropdown-item" href="#">Update Data</a></li>
+                                                                                                                                                        <div class="navbar-collapse justify-content-end" id="navbarNav">
+                                                                                                                                                            <ul class="navbar-nav">
+                                                                                                                                                                <li class="nav-item dropdown">
+                                                                                                                                                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                                                                                                                                                        data-bs-toggle="dropdown" aria-expanded="false">
+                                                                                                                                                                        Akun Saya
+                                                                                                                                                                    </a>
+                                                                                                                                                                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                                                                                                                                                        <li><a class="dropdown-item" href="#">Logout</a></li>
+                                                                                                                                                                        <li><a class="dropdown-item" href="#">Update Data</a></li>
+                                                                                                                                                                    </ul>
+                                                                                                                                                                </li>
                                                                                                                                                             </ul>
-                                                                                                                                                        </li>
-                                                                                                                                                    </ul>
-                                                                                                                                                </div>
-                                                                                                                                            -->
+                                                                                                                                                        </div>
+                                                                                                                                                    -->
         </div>
     </nav>
 
@@ -61,9 +61,9 @@
                 <div class="card">
                     <div class="card-body">
                         <!-- 03. Searching -->
-                        <form id="todo-form" action="" method="get">
+                        <form id="todo-form" action="{{ route('todo') }}" method="get">
                             <div class="input-group mb-3">
-                                <input type="text" class="form-control" name="search" value=""
+                                <input type="text" class="form-control" name="search" value="{{ request('search') }}"
                                     placeholder="masukkan kata kunci">
                                 <button class="btn btn-secondary" type="submit">
                                     Cari
