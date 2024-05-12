2cdcd7ed62751aeb04aa8daebb88c82b2c1507a9
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-05-12 22:08:08 +0700

    Feat: Add pagination UI

--- resources/views/todo/app.blade.php
+++ resources/views/todo/app.blade.php
@@ -6,21 +6,21 @@
         <div class="container-fluid col-md-7">
             <div class="navbar-brand">Simple To Do List</div>
             <!--
-                                                                                                                                                        <div class="navbar-collapse justify-content-end" id="navbarNav">
-                                                                                                                                                            <ul class="navbar-nav">
-                                                                                                                                                                <li class="nav-item dropdown">
-                                                                                                                                                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                                                                                                                                                                        data-bs-toggle="dropdown" aria-expanded="false">
-                                                                                                                                                                        Akun Saya
-                                                                                                                                                                    </a>
-                                                                                                                                                                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                                                                                                                                                        <li><a class="dropdown-item" href="#">Logout</a></li>
-                                                                                                                                                                        <li><a class="dropdown-item" href="#">Update Data</a></li>
-                                                                                                                                                                    </ul>
-                                                                                                                                                                </li>
-                                                                                                                                                            </ul>
-                                                                                                                                                        </div>
-                                                                                                                                                    -->
+                                                                                                                                                            <div class="navbar-collapse justify-content-end" id="navbarNav">
+                                                                                                                                                                <ul class="navbar-nav">
+                                                                                                                                                                    <li class="nav-item dropdown">
+                                                                                                                                                                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                                                                                                                                                            data-bs-toggle="dropdown" aria-expanded="false">
+                                                                                                                                                                            Akun Saya
+                                                                                                                                                                        </a>
+                                                                                                                                                                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                                                                                                                                                            <li><a class="dropdown-item" href="#">Logout</a></li>
+                                                                                                                                                                            <li><a class="dropdown-item" href="#">Update Data</a></li>
+                                                                                                                                                                        </ul>
+                                                                                                                                                                    </li>
+                                                                                                                                                                </ul>
+                                                                                                                                                            </div>
+                                                                                                                                                        -->
         </div>
     </nav>
 
@@ -121,6 +121,7 @@
                                 </li>
                             @endforeach
                         </ul>
+                        {{ $tasks->links() }}
                     </div>
                 </div>
             </div>
