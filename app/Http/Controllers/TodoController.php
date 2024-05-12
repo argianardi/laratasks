9a32f9f49ea5432d8c9f1768225bbdb98ca659d9
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-05-12 14:56:26 +0700

    Feat: Add get and post controller

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tasks = todo::orderBy('task', 'asc')->get();
+        return view('todo.app', ['tasks' => $tasks]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'task' => 'required|min:3|max:25'
+        ], [
+            'task.required' => 'Input task wajib diisi',
+            'task.min' => 'Input task minimal 3 karakter',
+            'task.max' => 'Input task maksimal 25 karakter'
+        ]);
+
+        todo::create([
+            'task' => $request->task
+        ]);
+        return redirect('todo')->with('success', 'Task berhasil disimpan');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(todo $todo)
+    {
+        //
+    }
+}
